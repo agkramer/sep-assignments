ff9589c7cd7d5f38078e6aa01ad8e81ee4266d54
Allen Kramer <kramer.alleng@gmail.com>
2016-12-21 07:48:33 -0500

    added code for node.rb initialize function

--- 01-data-structures/06-trees/binary_tree/node.rb
+++ 01-data-structures/06-trees/binary_tree/node.rb
@@ -1,9 +1,10 @@
 class Node
-  attr_accessor :title
-  attr_accessor :rating
-  attr_accessor :left
-  attr_accessor :right
+  attr_accessor :title, :rating, :left, :right
 
   def initialize(title, rating)
+    self.title = title
+    self.rating = rating
+    self.left = nil
+    self.right = nil
   end
 end
